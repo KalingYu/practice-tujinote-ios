bd28e848e203c552b50d8eacbed0ede5def3c118
Kaling <yujialingbbs@163.com>
2016-06-07 11:42:42 +0800

    Initial Commit

---
+++ TujiNote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TujiNote
+//
+//  Created by 余嘉陵 on 6/7/16.
+//  Copyright © 2016 Kaling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
